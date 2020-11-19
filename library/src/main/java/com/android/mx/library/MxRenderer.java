0eb4281da433ce124527a3d59e6983110a63dc67
hlx <hanliuxin5@users.noreply.github.com>
2020-11-19 18:56:36 +0800

    Update MxRenderer.java
    avoid exception

--- library/src/main/java/com/android/mx/library/MxRenderer.java
+++ library/src/main/java/com/android/mx/library/MxRenderer.java
@@ -289,6 +289,7 @@ class MxRenderer implements GLTextureView.Renderer, SurfaceTexture
                     triangleVerticesData.length * FLOAT_SIZE_BYTES)
                     .order(ByteOrder.nativeOrder()).asFloatBuffer();
         }
+        triangleVertices.position(0);
         triangleVertices.put(triangleVerticesData).position(0);
         float videoRadio = videoWidth * 1.0f / videoHeight;
         float viewRadio = viewWidth * 1.0f / viewHeight;
